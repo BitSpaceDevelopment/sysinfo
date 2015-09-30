ea79d3cd274a03c8fb485c5d545593041d5dfbbf
Dann Blair <dann@bitspacedevelopment.com>
2015-09-30 10:03:53 -0500

    Added gh-pages content

--- background.js
+++
@@ -1,4 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('window.html',
-    {minWidth: 630, minHeight: 650});
-});
--- js/options.js
+++
@@ -1,5 +0,0 @@
-// Save API click event
-$(".api-btn").click(function(){
-    var data = document.getElementById("api_key_input").value;
-    localStorage["api"] = data; 
-});
